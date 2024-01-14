186470954ded22fc795b2465cbba90495920f8dc
윤건수 <ygs3004@naver.com>
2024-01-14 23:57:34 +0900

    [2024/01/14] Thymeleaf 반복문 처리

---
+++ ex3/src/main/java/com/yexample/ex3/dto/SampleDTO.java
@@ -0,0 +1,17 @@
+package com.yexample.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
+}
