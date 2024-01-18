60dbd196d1bbab44380033f0226b281af4eb98eb
윤건수 <ygs3004@naver.com>
2024-01-18 22:27:09 +0900

    [2024/01/18] Thymeleaf - layout 작성법   > replace   > insert   > fragment   > 파라미터 전달

--- ex3/src/main/java/com/yexample/ex3/controller/SampleController.java
+++ ex3/src/main/java/com/yexample/ex3/controller/SampleController.java
@@ -60,8 +60,8 @@ public class SampleController {
         log.info("ex3");
     }
 
-    @GetMapping("/exLayout1")
-    public void exLayout123(){
+    @GetMapping({"/exLayout1", "/exLayout2", "exTemplate"})
+    public void exLayout1(){
         log.info("exLayout...............");
     }
 
