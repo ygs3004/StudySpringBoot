cb26739b17b02ebda1c7bda027021cdab50f568e
윤건수 <ygs3004@naver.com>
2024-01-23 00:55:50 +0900

    [2024/01/23] GuestBook Project Init

---
+++ guestbook/src/test/java/com/yexample/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yexample.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
