24aff9f45f07755420f658f091b42957fffce3ee
윤건수 <ygs3004@naver.com>
2024-01-27 00:11:14 +0900

    [2024/01/27]  - PageResultDTO Paging2

--- guestbook/src/main/java/com/yexample/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/yexample/guestbook/controller/GuestbookController.java
@@ -1,19 +1,31 @@
 package com.yexample.guestbook.controller;
 
+import com.yexample.guestbook.dto.PageRequestDTO;
+import com.yexample.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("list..............");
-        return "/guestbook/list";
+    private final GuestbookService guestbookService;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list.............." + pageRequestDTO);
+        model.addAttribute("result", guestbookService.getList(pageRequestDTO));
     }
 
 }
