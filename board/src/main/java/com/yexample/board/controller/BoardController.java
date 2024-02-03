9686de622aa5b3c2b7e1539a35220d1e93a31db4
윤건수 <ygs3004@naver.com>
2024-02-04 03:50:07 +0900

    [2024/02/04]  - Board Register/Read API && Template

--- board/src/main/java/com/yexample/board/controller/BoardController.java
+++ board/src/main/java/com/yexample/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.yexample.board.controller;
 
+import com.yexample.board.dto.BoardDTO;
 import com.yexample.board.dto.PageRequestDTO;
 import com.yexample.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +25,28 @@ public class BoardController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list............" + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("list............");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+        log.info("list............" + boardDTO);
+        Long bno = boardService.register(boardDTO);
+        log.info("BNO: " + bno);
+        redirectAttributes.addFlashAttribute("msg", bno);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO requestDTO, Long bno, Model model) {
+        log.info("bno: " + bno);
+        BoardDTO boardDTO = boardService.get(bno);
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
     }
 
 }
