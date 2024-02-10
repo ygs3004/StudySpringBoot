1e3f4181309440cc7179b5c3070d18b21dd8dc7b
윤건수 <ygs3004@naver.com>
2024-02-10 15:15:58 +0900

    [2024/02/10]  - Movie Review Project init

---
+++ mreview/src/test/java/com/yexample/mreview/MreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yexample.mreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
