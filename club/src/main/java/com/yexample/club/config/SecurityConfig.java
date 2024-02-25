1b06769aea56311294838083699b78e67603319a
윤건수 <ygs3004@naver.com>
2024-02-25 16:33:22 +0900

    [2024/02/24] Club(Security) Project  - Security Login simple config

---
+++ club/src/main/java/com/yexample/club/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.yexample.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user1")
+                .password(passwordEncoder().encode("1"))
+                .roles("USER")
+                .build();
+
+        log.info("userDetailService.............................");
+        log.info(user);
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/sample/all").permitAll();
+            auth.requestMatchers("/sample/member").hasRole("USER");
+        });
+
+        // httpSecurity.formLogin(); -> Deprecate
+        httpSecurity.formLogin(formLogin -> {
+            formLogin
+                    .usernameParameter("username")
+                    .passwordParameter("password");
+        });
+
+        // 세션마다 생성되는 토큰값 disabled
+        // 외부에서의 API 요청을 인정할때는 disabled
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ club/src/test/java/com/yexample/club/security/PasswordTests.java
@@ -0,0 +1,23 @@
+package com.yexample.club.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testEncode() {
+        String password = "1";
+        String enPw = passwordEncoder.encode(password);
+        System.out.println("enPw: " + enPw);
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+        System.out.println("matchResult: " + matchResult);
+    }
+
+}
