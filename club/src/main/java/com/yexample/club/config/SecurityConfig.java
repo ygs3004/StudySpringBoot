298964f0f0b0f87fe8b630bec560b6c6eeab6087
윤건수 <ygs3004@naver.com>
2024-03-02 03:35:07 +0900

    [2024/03/02] Club(Security) Project  - @EnableMethodSecurity  - @PreAuthorize  URL 권한체크

--- club/src/main/java/com/yexample/club/config/SecurityConfig.java
+++ club/src/main/java/com/yexample/club/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 @Log4j2
 public class SecurityConfig {
 
@@ -26,11 +28,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/sample/all").permitAll();
-            auth.requestMatchers("/sample/member").hasRole("USER");
-            auth.requestMatchers("/sample/admin").hasRole("ADMIN");
-        });
+//        httpSecurity.authorizeHttpRequests(auth -> {
+//            auth.requestMatchers("/sample/all").permitAll();
+//            auth.requestMatchers("/sample/member").hasRole("USER");
+//            auth.requestMatchers("/sample/admin").hasRole("ADMIN");
+//        });
 
         // httpSecurity.formLogin(); -> Deprecate
         httpSecurity.formLogin(formLogin -> {
--- club/src/main/java/com/yexample/club/controller/SampleController.java
+++ club/src/main/java/com/yexample/club/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.yexample.club.controller;
 
 import com.yexample.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
 
     // 비 로그인 유저
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
         log.info("exAll .......................");
@@ -27,9 +29,19 @@ public class SampleController {
     }
 
     // 관리자
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
         log.info("exAdmin .......................");
         log.info("clubAuthMemberDTO: " + clubAuthMemberDTO);
+    }
+
+    @PreAuthorize("#clubAuthMemberDTO != null && #clubAuthMemberDTO.username eq \"user75@yexample.com\"")
+    @GetMapping("/exOnly")
+    public String exOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
+        log.info("exAdmin .......................");
+        log.info("clubAuthMemberDTO: " + clubAuthMemberDTO);
+
+        return "/sample/admin";
     }
 }
