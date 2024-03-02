805fdb55830f0daddde66278835e1c1a0575cb7d
윤건수 <ygs3004@naver.com>
2024-03-02 22:54:53 +0900

    [2024/03/02] Bimovie Project(양방향 참조)   - init

---
+++ bimovie/src/main/java/com/yexample/bimovie/BimovieApplication.java
@@ -0,0 +1,13 @@
+package com.yexample.bimovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimovieApplication.class, args);
+	}
+
+}
---
+++ bimovie/src/test/java/com/yexample/bimovie/BimovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yexample.bimovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
